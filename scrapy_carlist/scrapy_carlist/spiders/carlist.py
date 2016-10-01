0020dbbccbf498e93ec5abe6118832557c390343
Anas Juwaidi <anas.didi95@gmail.com>
2016-10-01 18:32:24 +0800

    Define function to follow pagination links

--- scrapy_carlist/scrapy_carlist/spiders/carlist.py
+++ scrapy_carlist/scrapy_carlist/spiders/carlist.py
@@ -10,7 +10,6 @@ class SpiderCarlist(scrapy.Spider):
     name = 'carlist' # spider name
 
     # list of site to crawl (top-level, first page)
-    # respective to global list MANUFACTURER
     start_urls = ['http://www.carlist.my/car/toyota']
 
     def parse(self, response):
@@ -26,12 +25,22 @@ class SpiderCarlist(scrapy.Spider):
         for href in top.css('h2 a::attr(href)').extract():
             yield scrapy.Request(url=response.urljoin(href), callback=self.parse_car)
 
+        # follow pagination links
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            global COUNT # used global variable
+            COUNT += 1 # add counter
+            yield scrapy.Request(url=next_page, callback=self.parse)
+
     # get data for each page and extract
     def parse_car(self, response):
         # get label for car details
         label = response.css('div.listing__key-listing__list p span.list-item__title::text').extract()
         # get value for car details
         value = response.css('div.listing__key-listing__list p span.float--right::text').extract()
+        # get price for car
+        price = response.css('div.grid__item p.listing__price::text').extract_first()
 
         # combine both label and value to dictionary object
         data = dict(izip(label, value));
@@ -46,5 +55,6 @@ class SpiderCarlist(scrapy.Spider):
             'Seat Capacity': data['Seat Capacity'],
             'Mileage': data['Mileage'],
             'Car type': data['Car type'],
-            'Colour': data['Colour']
+            'Colour': data['Colour'],
+            'Price': price
         }
